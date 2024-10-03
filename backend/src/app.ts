4b8d22f7580520506fd8040626c1c5de9bbe5453
Muskan Bharti <muskan_ug_23@civil.nits.ac.in>
2024-10-03 08:01:33 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://modern-api-chat-full-stack-1-frontend.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
